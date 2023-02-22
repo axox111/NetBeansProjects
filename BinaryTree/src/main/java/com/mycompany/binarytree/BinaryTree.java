5ee15e3e62ee8655816b7d7c4b0c1948f6da3afa
Vladislav <gggwp4@gmail.com>
2023-02-22 14:55:38 +0300

    insert correction

--- BinaryTree/src/main/java/com/mycompany/binarytree/BinaryTree.java
+++ BinaryTree/src/main/java/com/mycompany/binarytree/BinaryTree.java
@@ -4,10 +4,10 @@ public class BinaryTree {
 
     public static void main(String[] args) {
         Tree tree = new Tree();
-        int[] arr_to_sort = {7, 5, 8, 4, 2, 387, 24, 325, 5};
+        int[] arr_to_sort = {100, 90, 95, 80, 110, 105, 104, 115, 85, 84};
         for (int val : arr_to_sort){
             Leaf leaf = new Leaf(val);
-            tree.insert(leaf);
+            tree.insertTwo(leaf);
         }
         tree.printTree();
     }
--- BinaryTree/src/main/java/com/mycompany/binarytree/Tree.java
+++ BinaryTree/src/main/java/com/mycompany/binarytree/Tree.java
@@ -28,6 +28,32 @@ public class Tree {
         }
     }
 
+    public void insertTwo(Leaf val) {
+        Leaf newLeaf = val;
+        if (this.root == null) {
+            this.root = newLeaf;
+        } else {
+            Leaf currentLeaf = this.root;
+            getBranch(currentLeaf, newLeaf);
+        }
+    }
+
+    public void getBranch(Leaf currLeaf, Leaf newLeaf) {
+        if (newLeaf.val < currLeaf.val) {
+            if (currLeaf.left == null) {
+                currLeaf.left = newLeaf;
+            } else {
+                getBranch(currLeaf.left, newLeaf);
+            }
+        } else {
+            if (currLeaf.right == null) {
+                currLeaf.right = newLeaf;
+            } else {
+                getBranch(currLeaf.right, newLeaf);
+            }
+        }
+    }
+
     public void printTree() {
         System.out.println(this.root.val);
         if (this.root.left != null) {
