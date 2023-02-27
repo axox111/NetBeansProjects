3a01ea8baed96af0528741999892491ac9ac9d9f
Vladislav <gggwp4@gmail.com>
2023-02-27 11:06:00 +0300

    treeSorting

--- BinaryTree/src/main/java/com/mycompany/binarytree/BinaryTree.java
+++ BinaryTree/src/main/java/com/mycompany/binarytree/BinaryTree.java
@@ -4,11 +4,11 @@ public class BinaryTree {
 
     public static void main(String[] args) {
         Tree tree = new Tree();
-        int[] arr_to_sort = {100, 90, 95, 80, 110, 105, 104, 115, 85, 84};
+        int[] arr_to_sort = {100, 95, 90, 80, 110, 105, 104, 115, 85, 84};
         for (int val : arr_to_sort){
             Leaf leaf = new Leaf(val);
-            tree.insertTwo(leaf);
+            tree.insert(leaf);
         }
-        tree.printTree();
+        tree.sorting(tree.root);
     }
 }
--- BinaryTree/src/main/java/com/mycompany/binarytree/Tree.java
+++ BinaryTree/src/main/java/com/mycompany/binarytree/Tree.java
@@ -14,26 +14,6 @@ public class Tree {
             this.root = newLeaf;
         } else {
             Leaf currentLeaf = this.root;
-            if (newLeaf.val < currentLeaf.val) {
-                while (currentLeaf.left != null) {
-                    currentLeaf = currentLeaf.left;
-                }
-                currentLeaf.left = newLeaf;
-            } else {
-                while (currentLeaf.right != null) {
-                    currentLeaf = currentLeaf.right;
-                }
-                currentLeaf.right = newLeaf;
-            }
-        }
-    }
-
-    public void insertTwo(Leaf val) {
-        Leaf newLeaf = val;
-        if (this.root == null) {
-            this.root = newLeaf;
-        } else {
-            Leaf currentLeaf = this.root;
             getBranch(currentLeaf, newLeaf);
         }
     }
@@ -54,21 +34,11 @@ public class Tree {
         }
     }
 
-    public void printTree() {
-        System.out.println(this.root.val);
-        if (this.root.left != null) {
-            Leaf value = this.root;
-            while (value.left != null) {
-                System.out.println(value.left.val);
-                value = value.left;
-            }
-        }
-        if (this.root.right != null) {
-            Leaf value = this.root;
-            while (value.right != null) {
-                System.out.println(value.right.val);
-                value = value.right;
-            }
+    public void sorting(Leaf leaf) {
+        if (leaf != null) {
+            sorting(leaf.left);
+            System.out.print(leaf.val + " ");
+            sorting(leaf.right);
         }
     }
 }
